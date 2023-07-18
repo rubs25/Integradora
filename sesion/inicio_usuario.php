6cbc9f8c68cb2affe13f66a40baa20494ecdcf17
Ruben Azpeitia <rubenc1148@gmail.com>
2023-07-18 17:13:01 -0600

    cambios de inicio

--- sesion/inicio_usuario.php
+++ sesion/inicio_usuario.php
@@ -1,5 +1,43 @@
 <?php
 session_start();
+include '../basedatos/conexion.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nombre = $_POST['nombre'];
+    $contrasena = $_POST['contrasena'];
+
+    // Consulta para verificar las credenciales de inicio de sesión
+    $query = "SELECT * FROM t_user WHERE nombre = '$nombre' AND contrasena = '$contrasena'";
+    $result = mysqli_query($conexion, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        
+        $user = mysqli_fetch_assoc($result);
+        
+        $_SESSION['id'] = $user['id'];
+
+        // Redireccionar al catálogo o a otra página según el tipo de usuario
+        if ($user['nombre'] === 'root' || $user['nombre'] === 'root') {
+        
+            header("Location: ../index.php");
+        
+        } else {
+
+            header("Location: ../registro.php");
+        
+        }
+        exit();
+
+    } else {
+        
+        $error_message = 'Credenciales inválidas. Por favor, intenta nuevamente.';
+    }
+}
+?>
+
+
+<!-- <?php
+session_start();
 include("../basedatos/conexion.php");
 
 
@@ -10,9 +48,10 @@ if (isset($_POST['inicio_usuario']))
    $query = "SELECT * FROM t_user WHERE nombre = '$nombre' AND contrasena = '$contrasena'";
    $validar_login = mysqli_query($conexion,$query);
 
+    
     if (mysqli_num_rows($validar_login)>0) {
 
-        $_SESSION['usuario']=$nombre;
+        $_SESSION['id']=$nombre;
         header("location:../bienvenida.php");
         exit(); 
         echo ' 
@@ -31,4 +70,4 @@ if (isset($_POST['inicio_usuario']))
         ';
         exit();
     }   
-?>
+?> -->
