846b791498bd12ff02830416cc7192810215e739
Adan Chavez OLivera <adanch22@gmail.com>
2023-07-22 00:03:42 -0600

    cambie el nombre del campo  id_user

--- admin/delete.php
+++ admin/delete.php
@@ -4,7 +4,7 @@ include("conecction.php");
 
 if(isset($_GET['id'])) {
   $id = $_GET['id'];
-  $query = "DELETE FROM t_user WHERE id = $id";
+  $query = "DELETE FROM t_user WHERE id_user = $id";
   $result = mysqli_query($conexion, $query);
   if(!$result) {
     die("Query Failed.");
