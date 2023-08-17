1b3bf188c1d0440cfb92331ee6edb5a5680aa0f1
Ruben Azpeitia <rubenc1148@gmail.com>
2023-08-16 18:55:26 -0600

    cambios

--- admin/factura.php
+++ admin/factura.php
@@ -1,64 +1,58 @@
 <?php
 require('fpdf/fpdf.php');
 
-class PDF extends FPDF
-{
-    function Header()
-    {
-        $this->Image('logos2.jpg',60,160,90); // Ajusta las coordenadas de la imagen
-        $this->SetFont('Arial','B',15);
-        $this->Cell(0,10,'Factura',0,1,'C');
-    }
-
-    function Footer()
-    {
-        $this->SetY(-15);
-        $this->SetFont('Arial','I',8);
-        $this->Cell(0,10,utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'C');
-    }
-}
-
-$pdf = new PDF('P');
-$pdf->AliasNbPages();
-$pdf->AddPage();
-$pdf->SetFont('Arial','',12);
-
+// Obtener los datos de la tabla de detalles de ventas
 $servername = "localhost";
 $username = "root";
 $password = "Rubas2509";
 $dbname = "integradora2";
 
+// Crear la conexión a la base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verificar la conexión
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM detalles_ventas";
+// Crear una instancia de la clase FPDF
+$pdf = new FPDF();
+$pdf->AddPage();
+
+// Establecer la fuente y el estilo del encabezado
+$pdf->SetFont('Arial','B',14);
+$pdf->Cell(40,10,'Facturación',0,1,'C');
+
+// Establecer la fuente y el estilo del contenido
+$pdf->SetFont('Arial','',12);
+
+// Consulta a la tabla de detalles de ventas
+$sql = "SELECT nombre_producto, cantidad, precio, subtotal FROM detalles_ventas";
 $result = $conn->query($sql);
 
+// Verificar si hay registros en la tabla
 if ($result->num_rows > 0) {
-    $total = 0;
+    // Crear la cabecera de la tabla
+    $pdf->Cell(60,10,'Nombre del Producto',1,0,'C');
+    $pdf->Cell(40,10,'Cantidad',1,0,'C');
+    $pdf->Cell(40,10,'Precio',1,0,'C');
+    $pdf->Cell(50,10,'Subtotal',1,1,'C');
 
-    while($row = $result->fetch_assoc()) {
-        $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(0,10,'Descripción: ' . $row["nombre_producto"],0,1,'L');
-        $pdf->Cell(0, 10, 'Cantidad: ' . $row["cantidad"], 0, 1, 'L');
-        $pdf->Cell(0,10,'IVA: ' . $row["iva"],0,1,'L');
-        $pdf->Cell(0,10,'Subtotal: ' . $row["subtotal"],0,1,'L');
-        $pdf->Cell(0,10,'Descuento: ' . $row["descuento"],0,1,'L');
-        
-        $total += $row["subtotal"];
+    // Mostrar los datos de la tabla
+    while ($row = $result->fetch_assoc()) {
+        $pdf->Cell(60,10,$row['nombre_producto'],1,0,'C');
+        $pdf->Cell(40,10,$row['cantidad'],1,0,'C');
+        $pdf->Cell(40,10,$row['precio'],1,0,'C');
+        $pdf->Cell(50,10,$row['subtotal'],1,1,'C');
     }
-
-    $pdf->SetFont('Arial','B',12);
-    $pdf->Cell(0,10,'Total: $'.$total,0,1,'R');
 } else {
-    $pdf->Cell(0,10,'No se encontraron resultados.',0,1,'C');
+    // No se encontraron registros en la tabla
+    $pdf->Cell(0,10,'No se encontraron detalles de ventas.',0,1,'C');
 }
 
+// Cerrar la conexión a la base de datos
 $conn->close();
 
-$pdf->Output('factura.pdf', 'D');
-
+// Salida del archivo PDF
+$pdf->Output('facturacion.pdf', 'D');
 ?>
